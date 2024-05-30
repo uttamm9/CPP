4ec788d61ea00613f336174989e41441df8f8f98
Uttam sharma <uttamgoutam>
2024-05-30 11:45:59 +0530

    do wile loop in CPP

---
+++ basic/DoWhiel.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()  //do ehile loop run atlist 1 time;
+{
+    int num;
+    num=0;
+    do{ // this part run atleast 1 time;
+        cout<<"num value is num "<<num<<endl;
+        num=num+1;
+    }while(num>885);//if condtion is true then this part will bhi run;
+    return 0;
+}
