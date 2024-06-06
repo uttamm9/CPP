77656d6aa87a0af78bddde02445f6a6e6571d370
Uttam sharma <uttamgoutam>
2024-06-06 22:10:40 +0530

    array diclaration

---
+++ array/array.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int array[4]={10,20,30,40};  /*array disclaration [4] it is size of array
+                                   it mean thare is 4 diftrant data */
+    cout<<array[3];
+    //array[0] is mean first data of array; array diclare start frome [0][1]...
+    return 0;
+}
